ef16f8b185f830d2595f4efc37e8d1e398cdacf3
Marcelo Cairuga <marcelocairuga@ifsul.edu.br>
2025-05-27 13:11:16 -0300

    inicial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+   res.json({message: "Bem-vindo a API Tarefas"});
+});
+
+app.listen(port, () => {
+  console.log(`API rodando em http://localhost:${port}`);
+});
